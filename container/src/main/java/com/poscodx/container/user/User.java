3dd5b5fa7fdf7f96ddfef4fb84a53d57a648d322
shin-dongheon <tlsejdgjs@naver.com>
2023-10-04 11:21:53 +0900

    container #2: user

--- container/src/main/java/com/poscodx/container/user/User.java
+++ container/src/main/java/com/poscodx/container/user/User.java
@@ -4,9 +4,30 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class User {
+	private Long no = 0L;
 	private String name = "둘리";
+
 	
+	//오버로딩
+	public User(){
+		
+	}
+	public User(String name){
+		this.name = name;
+	}
+	public User(Long no,String name){
+		this.no = no;
+		this.name = name;
+	}
 	public String getName() {
 		return name;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"no=" + no +
+				", name='" + name + '\'' +
+				'}';
 	}
 }
--- container/src/main/java/com/poscodx/container/user/test/XmlConfigTest.java
+++ container/src/main/java/com/poscodx/container/user/test/XmlConfigTest.java
@@ -51,8 +51,18 @@ public class XmlConfigTest {
 		System.out.println(user.getName());
 
 		// Type으로 빈 가져오기
-		user = ac.getBean(User.class);
-		System.out.println(user.getName());
+		// 같은 타입의 빈이 2개 이상 있으면 Type으로 가져오기는 실패
+		user = ac.getBean("user2",User.class);
+		System.out.println(user);
+
+		//	파라미터 2개인 생성자로 생성된 빈 가져오기 I
+		user = ac.getBean("user3",User.class);
+		System.out.println(user);
+
+		//	파라미터 2개인 생성자로 생성된 빈 가져오기 II
+		user = ac.getBean("user4",User.class);
+		System.out.println(user);
+
 
 	}
 	
